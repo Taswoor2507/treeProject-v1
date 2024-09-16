2da51a6b973a6a83a4cd2a5bdc40fff034ad9605
Taswoor Hussain <taswoorhossein72@gmail.com>
2024-09-16 23:22:27 +0500

    setup front end also configure shade cn ui libraly for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button"
+const App = () => {
+  return (
+    <div className="p-6">
+      <Button>Shafde cn button</Button>
+    </div>
+  )
+}
+
+export default App
