88eb03ed0044869d16fdd9f0b562ff1f515d6f99
Taswoor Hussain <taswoorhossein72@gmail.com>
2024-09-20 23:51:45 +0500

    changes

--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -1,10 +1,11 @@
 import express from "express"
+import verifyJWT from "../middlewares/auth.middleware.js";
+import authRole from "../middlewares/roleBaseAuth.middleware.js";
 import { addComment, deleteComment, getAllComments, getTreeComments } from "../controlllers/comments.controller.js";
 const router = express.Router();
-import verifyJWT from "../middlewares/auth.middleware.js";
 
 router.route("/add-comment").post(verifyJWT,addComment);
 router.route("/get-all-comments/:treeId").get(getTreeComments);
-router.route("/all").get(getAllComments);
-router.route("/delete/:commentId").delete(deleteComment);
+router.route("/all").get(verifyJWT , authRole("admin"), getAllComments);
+router.route("/delete/:commentId").delete(verifyJWT , authRole("admin"), deleteComment);
 export default router;
--- src/routes/tree.route.js
+++ src/routes/tree.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import authRole from "../middlewares/roleBaseAuth.middleware.js";
+import verifyJWT from "../middlewares/auth.middleware.js";
 import { addTree, deleteTree, getAllTrees, getTreeById, updateTree } from "../controlllers/tree.controller.js";
 const router = express.Router();
 
 router.route("/add-tree").post(addTree)
 router.route("/all").get(getAllTrees)
 router.route("/:treeId").get(getTreeById)
-router.route("/:treeId").patch(updateTree)
-router.route("/:treeId").delete(deleteTree)
+router.route("/:treeId").patch(verifyJWT , authRole("admin"), updateTree)
+router.route("/:treeId").delete(verifyJWT , authRole("admin"),deleteTree)
 export default router;
